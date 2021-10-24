f45db0d8e1209aa884ba02f0879a7e88c77d733c
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-10-24 14:16:12 +0530

    add: Layout, container components

---
+++ components/common/Layout.jsx
@@ -0,0 +1,66 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Container from './Container'
+
+/**
+ * Use this component as a page layout.
+ * Every page should contain this layout component.
+ *
+ * @children children JSX Elements
+ * @title SEO title
+ * @navbar boolean (hide or show)
+ */
+const Layout = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <main className='font-montserrat text-black bg-offWhite min-h-screen grid grid-cols-1 grid-rows-4 md:grid-cols-4 md:grid-rows-1 gap-4'>
+        <div className='col-span-1 row-span-1 md:row-span-1 md:col-span-1 flex flex-col justify-between bg-maroon-base md:h-auto'>
+          <div>
+            <Container>
+              <div className='mb-3'>
+                <Image
+                  src='/patterns/line.svg'
+                  layout='intrinsic'
+                  width={690}
+                  height={6}
+                  alt='line'
+                />
+              </div>
+              <div className='h-full'>
+                <h1 className='font-semibold text-xl sm:text-2xl text-center md:text-left text-yellow-base mb-3 md:mb-64'>
+                  LANKAN URBAN DICTIONARY
+                </h1>
+                <p className='hidden md:block text-4xl text-right text-yellow-base'>
+                  ✺
+                </p>
+              </div>
+              <div className='mb-3'>
+                <Image
+                  src='/patterns/line.svg'
+                  layout='intrinsic'
+                  width={690}
+                  height={6}
+                  alt='line'
+                />
+              </div>
+              <p>(// here comes the button)</p>
+            </Container>
+          </div>
+          <div>
+            <div className='h-5 bg-orange' />
+            <div className='h-5 bg-green-base' />
+          </div>
+        </div>
+        <div className='col-span-1 row-span-3 md:row-span-1 md:col-span-3'>
+          {children}
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Layout
--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,43 +1,17 @@
+import Container from '../components/common/Container'
+import Layout from '../components/common/Layout'
 import Navbar from '../components/common/Navbar'
 const ProfilePage = () => {
   return (
-    <main className='flex'>
-      <aside className='w-2/5 min-h-screen bg-maroon-base'>
-        <h1>Lankan Urban Dictionary</h1>
-      </aside>
-
-      <section className='w-3/5 bg-offWhite px-20'>
-        <Navbar />
-        <section className='py-3 border-b-2 border-maroon-base flex justify-between'>
-          <h1 className='text-5xl text-maroon-base '>Saman Kumara</h1>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='mt-5 h-7 w-7 cursor-pointer stroke-current text-maroon-base'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth={2}
-              d='M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z'
-            />
-          </svg>
-        </section>
-        <section className='py-6'>
-          <h1 className='text-3xl text-maroon-base'>Bio</h1>
-          <h2 className='py-2 text-lg'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi vel
-            ea perspiciatis in atque, minima nisi accusamus quae doloribus id.
-          </h2>
-        </section>
-        <section className='py-2'>
-          <div className='flex justify-between'>
-            <h1 className='text-3xl text-maroon-base'>Your Definitions</h1>
+    <Layout title='Profile | LKUD'>
+      <Container>
+        <section className='bg-offWhite'>
+          <Navbar />
+          <section className='py-3 border-b-2 border-maroon-base flex justify-between'>
+            <h1 className='text-5xl text-maroon-base '>Saman Kumara</h1>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              className='mt-2 h-6 w-6 stroke-current text-maroon-base cursor-pointer'
+              className='mt-5 h-7 w-7 cursor-pointer stroke-current text-maroon-base'
               fill='none'
               viewBox='0 0 24 24'
               stroke='currentColor'
@@ -46,20 +20,46 @@ const ProfilePage = () => {
                 strokeLinecap='round'
                 strokeLinejoin='round'
                 strokeWidth={2}
-                d='M19 9l-7 7-7-7'
+                d='M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z'
               />
             </svg>
-          </div>
+          </section>
+          <section className='py-6'>
+            <h1 className='text-3xl text-maroon-base'>Bio</h1>
+            <h2 className='py-2 text-lg'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi vel
+              ea perspiciatis in atque, minima nisi accusamus quae doloribus id.
+            </h2>
+          </section>
+          <section className='py-2'>
+            <div className='flex justify-between'>
+              <h1 className='text-3xl text-maroon-base'>Your Definitions</h1>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='mt-2 h-6 w-6 stroke-current text-maroon-base cursor-pointer'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M19 9l-7 7-7-7'
+                />
+              </svg>
+            </div>
 
-          <div className='pt-4 grid grid-cols-1 gap-1'>
-            <h2>Word 1/ වචනෙ </h2>
-            <h2>Word 1/ වචනෙ </h2>
-            <h2>Word 1/ වචනෙ </h2>
-            <h2>Word 1/ වචනෙ </h2>
-          </div>
+            <div className='pt-4 grid grid-cols-1 gap-1'>
+              <h2>Word 1/ වචනෙ </h2>
+              <h2>Word 1/ වචනෙ </h2>
+              <h2>Word 1/ වචනෙ </h2>
+              <h2>Word 1/ වචනෙ </h2>
+            </div>
+          </section>
         </section>
-      </section>
-    </main>
+      </Container>
+    </Layout>
   )
 }
 
